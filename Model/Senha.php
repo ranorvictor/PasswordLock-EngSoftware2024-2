312f36cf9e72d46b471531700b015fc341b886f8
rafaelpacinii <rafael_pmv@hotmail.com>
2024-09-24 16:30:54 -0300

    Adicionando a função novaSenha no modelo de Senha.

--- Model/Senha.php
+++ Model/Senha.php
@@ -2,6 +2,13 @@
 
 namespace Model;
 
-class Senha {
+include ("../public/conexao.php");
 
+class Senha {
+  public function novaSenha($id, $apelido, $plataforma, $usuario, $senha){
+    global $mysqli;
+    $sql = "INSERT INTO senhas (id, apelido, plataforma, usuario, senha) VALUES ('$id', '$apelido', '$plataforma', '$usuario', '$senha')";
+    $result = $mysqli->query($sql);
+    return $result;
+  }
 }
