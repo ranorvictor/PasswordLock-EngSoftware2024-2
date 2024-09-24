e330df923e85e566b4e44672c20acb75b283331b
rafaelpacinii <rafael_pmv@hotmail.com>
2024-09-24 16:15:18 -0300

    Criando as rotas do projeto.

--- public/index.php
+++ public/index.php
@@ -1,1 +1,24 @@
 <?php
+
+require "../Model/Senha.php";
+
+$route = $_GET['route'] ?? '';
+
+if ($route == '') {
+  echo "<br>Bem-vindo ao sistema de senhas!";
+} else if ($route == 'novaSenha') {
+  echo "<br>Editar senha";
+} else if ($route == 'apagarSenha') {
+  echo "<br>Deletar senha";
+} else if ($route == 'listarSenhas') {
+  echo "<br>Listar senhas";
+} else if ($route == 'pesquisarSenha') {
+  echo "<br>Listar senhas";
+} else if ($route == 'editarSenha') {
+  echo "<br>Editar senha";
+} else {
+  http_response_code(404);
+  echo 'Page not found (Invalid route)';
+}
+
+echo $view->output($model);
