33514d3d02ae48f9aa949b5325445e026394392c
rafaelpacinii <rafael_pmv@hotmail.com>
2024-09-24 21:55:17 -0300

    Revert "Adicionando a função novaSenha no modelo de Senha."
    This reverts commit 312f36cf9e72d46b471531700b015fc341b886f8.

--- Model/Senha.php
+++ Model/Senha.php
@@ -2,13 +2,6 @@
 
 namespace Model;
 
-include ("../public/conexao.php");
-
 class Senha {
-  public function novaSenha($id, $apelido, $plataforma, $usuario, $senha){
-    global $mysqli;
-    $sql = "INSERT INTO senhas (id, apelido, plataforma, usuario, senha) VALUES ('$id', '$apelido', '$plataforma', '$usuario', '$senha')";
-    $result = $mysqli->query($sql);
-    return $result;
-  }
+
 }
